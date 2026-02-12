c205d12bc7f900aa302ced342545cabcd2a21c79
Pavel Litvinenko <gerasim13@gmail.com>
2026-02-12 23:54:45 +0300

    Revert "feat(stream): add SourceSegmentMeta and segment_meta_at() to Source trait"
    This reverts commit 0156b1632da68c4787dc4b46b9753dd35d60e4fb.

--- crates/kithara-decode/src/lib.rs
+++ crates/kithara-decode/src/lib.rs
@@ -27,7 +27,6 @@
 //! ```
 
 mod decoder;
-mod encoder_delay;
 mod error;
 mod factory;
 mod symphonia;
@@ -54,8 +53,6 @@ pub use apple::{Apple, AppleAac, AppleAlac, AppleConfig, AppleFlac, AppleMp3};
 // Generic wrapper
 #[doc(hidden)]
 pub use decoder::Decoder;
-// Encoder delay types
-pub use encoder_delay::{DelaySource, EncoderDelayInfo};
 // Error types
 pub use error::{DecodeError, DecodeResult};
 // Factory for runtime selection
