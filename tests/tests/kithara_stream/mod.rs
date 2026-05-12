d7616d9820fffbe27bc4fc2c5575ecd7314bf688
Pavel Litvinenko <gerasim13@gmail.com>
2026-05-12 11:49:21 +0300

    test(mod.rs): swap T1..T14 + legacy modules for new probe-based modules

--- tests/tests/kithara_stream/mod.rs
+++ tests/tests/kithara_stream/mod.rs
@@ -1,10 +1,7 @@
 //! Integration tests for kithara-stream
 
-mod architecture_progressive_download;
 #[cfg(not(target_arch = "wasm32"))]
 mod reader_seek_overflow;
 mod source;
 mod source_position_contract;
 mod sync_reader_basic_test;
-#[cfg(not(target_arch = "wasm32"))]
-mod timeline_source_of_truth;
